f2ae84b6a2cd81ff89b038ae00c37dd3f318eea4
“FGonett” <csunknown@163.com>
2024-07-13 15:02:42 +0800

    Site updated: 2024-07-13 15:02:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/679efaa8.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/679efaa8.html","posts/679efaa8.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
